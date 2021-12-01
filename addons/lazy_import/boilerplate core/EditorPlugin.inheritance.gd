8913439999952213a4d6c5d399ceab9ae40f4029
tsbraun1891 <tsbraun1891@gmail.com>
2021-12-01 18:17:02 -0500

    Revert "Attempt to remove addons"
    This reverts commit 8200de90453b6981b89e38611f84ac34ea8e1b21.

---
+++ addons/lazy_import/boilerplate core/EditorPlugin.inheritance.gd
@@ -0,0 +1,76 @@
+tool
+extends EditorPlugin
+
+
+#############################
+#  Lazy Plugin Boilerplate  #
+# PLEASE DO NOT MODIFY THIS #
+#############################
+
+
+const singleton_to_instance = preload("../singleton.gd")
+var singleton
+var plugin_scope
+
+
+func _enter_tree():
+	instantiate_singleton()
+
+func _exit_tree():
+	if singleton:
+		if singleton.AUTOLOAD_SINGLETON:
+			remove_autoload_singleton(singleton.DIR_NAME)
+
+		singleton.queue_free()
+	
+	if plugin_scope: plugin_scope.deferred_exit_tree()
+
+
+func instantiate_singleton():
+	# Instance
+	singleton = singleton_to_instance.new()
+	# Change the Node Name
+	singleton.set_name(singleton.DIR_NAME)
+	# Add the Node as a Singleton
+	get_tree().get_root().call_deferred("add_child", singleton)
+	# Start your plugin
+	singleton.connect("ready", self, "_on_singleton_ready")
+
+
+func _on_singleton_ready():
+	if not singleton:
+		print ("Plugin singleton is not loaded properly")
+	elif not check_editor_configuration():
+		singleton.error("Wrong configuration")
+	else:
+		plugin_scope.name = singleton.DIR_NAME
+		if singleton.AUTOLOAD_SINGLETON:
+			add_autoload_singleton(singleton.DIR_NAME, singleton.ADDON_DIR + "singleton.gd")
+		
+		if plugin_scope: plugin_scope.deferred_enter_tree()
+
+
+func check_editor_configuration() -> bool:
+	if singleton:
+		if singleton.name != singleton.DIR_NAME: # checks if this is the first instance
+			singleton.error("Singleton should be called '" + singleton.DIR_NAME+"' to be found, instead '"+singleton.name+"'")
+		elif singleton != get_tree().get_root().get_node(singleton.DIR_NAME):
+			singleton.error("Singleton is not properly instantiaded")
+		elif not get_editor_interface().is_plugin_enabled(singleton.DIR_NAME): # checks if the plugin is turned on
+			singleton.error("The plugin is not turned on! This should not be running!")
+		elif plugin_scope.get_script().get_path().get_base_dir() != "res://addons/" + singleton.DIR_NAME: # check if the plugin is properly located
+			singleton.error(plugin_scope.get_script().get_path().get_base_dir() + " should be res://addons/" + singleton.DIR_NAME)
+		elif Engine.get_version_info().major != singleton.MIN_GODOT_VERSION_MAYOR:
+			singleton.error("Mayor Godot Version is " + str(Engine.get_version_info().major) + " and should be " + str(singleton.MIN_GODOT_VERSION_MAYOR))
+		elif Engine.get_version_info().minor < singleton.MIN_GODOT_VERSION_MINOR:
+			singleton.error("Minor Godot Version is " + str(Engine.get_version_info().minor) + " and should be >= " + str(singleton.MIN_GODOT_VERSION_MINOR))
+		elif Engine.get_version_info().patch < singleton.MIN_GODOT_VERSION_PATCH:
+			singleton.error("Patch Godot Version is " + str(Engine.get_version_info().patch) + " and should be >= " + str(singleton.MIN_GODOT_VERSION_PATCH))
+		else:
+			for _plugin in singleton.REQUIRED_PLUGINS_DIR_NAMES:
+				if not get_editor_interface().is_plugin_enabled(_plugin):
+					singleton.error(_plugin + " plugin is required to run this plugin!")
+					return false
+			return true
+	
+	return false
